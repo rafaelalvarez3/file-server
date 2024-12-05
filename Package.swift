8bd64c09b35e8a9fd9253a049003945077197322
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-12-05 11:44:48 -0600

    added the swift argument parser package

--- Package.swift
+++ Package.swift
@@ -5,13 +5,15 @@ import PackageDescription
 let package = Package(
     name: "file-server",
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.5.0")
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.5.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
     ],
     targets: [
         .executableTarget(
             name: "file-server",
             dependencies: [
                 .product(name: "Hummingbird", package: "hummingbird"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
             path: "Sources"),
     ]
